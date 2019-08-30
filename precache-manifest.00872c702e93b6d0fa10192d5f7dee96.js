3c5b43eb5d85089659e0aee4b41a21b965ae4a61
Rostyslav Yurchyk <ros_ur@ukr.net>
2019-08-30 11:08:49 +0300

    Updates

---
+++ precache-manifest.00872c702e93b6d0fa10192d5f7dee96.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "771820bb3bc24bb551538190c9f2fefe",
+    "url": "/syncher/index.html"
+  },
+  {
+    "revision": "3065783274d9fdb6057b",
+    "url": "/syncher/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "8425c9a27c3912fb1bb2",
+    "url": "/syncher/static/js/2.678f99dc.chunk.js"
+  },
+  {
+    "revision": "3065783274d9fdb6057b",
+    "url": "/syncher/static/js/main.76f3deed.chunk.js"
+  },
+  {
+    "revision": "8ebcaf64fe20ae0e5833",
+    "url": "/syncher/static/js/runtime~main.1b7b1c2e.js"
+  }
+]);
