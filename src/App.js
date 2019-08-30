55a8f2079b1df4012fd5546431796ff5eff10f13
Rostyslav Yurchyk <ros_ur@ukr.net>
2019-08-30 10:55:20 +0300

    switched to github gists as data source

--- src/App.js
+++ src/App.js
@@ -3,10 +3,19 @@ import styled from 'styled-components';
 import Reqs from './Reqs';
 import Synchs from './Synchs';
 
+const Wrap = styled.div`
+    background-color: #282c34;
+`;
+
 const Div = styled.div`
     color: #fff;
     text-align: center;
     background-color: #282c34;
+    min-height: 100vh;
+    @media (min-width: 900px) {
+        width: 70%;
+        margin: 0 auto;
+    }
 `;
 
 const Upd = styled.div`
@@ -25,15 +34,17 @@ function App() {
     const [lastUpd, setLastUpd] = useState('');
 
     const getData = async () => {
-        const res = await fetch('http://127.0.0.1:4321/chgkinfo', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'text/plain',
-            },
-            body: 'edb8e45f64b03144b39cd91b0678e3e1',
-        });
+        const res = await fetch('https://api.github.com/gists/c3f3f735a28b010bfcbff2ecf56c72a9');
+        // const res = await fetch('http://127.0.0.1:4321/chgkinfo', {
+        //     method: 'POST',
+        //     headers: {
+        //         'Content-Type': 'text/plain',
+        //     },
+        //     body: 'edb8e45f64b03144b39cd91b0678e3e1',
+        // });
         const data = await res.json();
-        const { updated, yourReqs, synchs } = data;
+        // console.log(JSON.parse(data.files['chgk-data.json'].content));
+        const { updated, yourReqs, synchs } = JSON.parse(data.files['chgk-data.json'].content);
         setLastUpd(updated);
         setRequests(yourReqs);
         setTourns(synchs);
@@ -44,13 +55,17 @@ function App() {
     }, []);
 
     return (
-        <Div className="App">
-            <Header className="App-header">Synchs</Header>
-            <Upd>Last update: {lastUpd}</Upd>
-            <hr />
-            <Reqs requests={requests} />
-            <Synchs tourns={tourns} />
-        </Div>
+        <Wrap>
+            <Div className="App">
+                <Header className="App-header">Synchs</Header>
+                <Upd>Last update: {lastUpd}</Upd>
+                <hr />
+                <main>
+                    <Reqs requests={requests} />
+                    <Synchs tourns={tourns} />
+                </main>
+            </Div>
+        </Wrap>
     );
 }
 
--- src/Synchs.jsx
+++ src/Synchs.jsx
@@ -2,16 +2,60 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 
 const Ul = styled.ul`
-    margin: 20px 0;
+    margin: 20px 0 0 0;
     padding: 0;
     list-style-type: none;
 `;
 
 const Li = styled.li`
-    margin: 10px 5px;
+    margin: 10px 5px 0 5px;
     padding: 5px;
     border: 1px solid goldenrod;
     border-radius: 20px;
+`;
+
+const Editor = styled.div`
+    font-size: 0.9em;
+    width: 60%;
+    margin: 0 auto;
+    text-align: left;
+    @media (max-width: 700px) {
+        width: 95%;
+    }
+`;
+
+const Info = styled.div`
+    color: goldenrod;
+    font-weight: 500;
+`;
+
+const H = styled.h3`
+    margin: 0;
+    padding: 0;
+`;
+
+const Input = styled.input`
+    background-color: #282c34;
+    color: #fff;
+    border-radius: 20px;
+    border: 2px solid goldenrod;
+    padding: 5px 10px;
+    font-size: 1.1em;
+`;
+
+const Links = styled.div`
+    display: flex;
+    justify-content: space-evenly;
+`;
+
+const A = styled.a`
+    text-decoration: none;
+    color: #99f;
+    &:hover,
+    :active {
+        background-color: #fff;
+        color: #000;
+    }
 `;
 
 function Synchs(props) {
@@ -22,9 +66,9 @@ function Synchs(props) {
     return (
         <>
             <label>
-                Search:
-                <input
+                <Input
                     type="text"
+                    placeholder="SEARCH"
                     onChange={event => {
                         setQuery(event.target.value);
                     }}
@@ -32,9 +76,26 @@ function Synchs(props) {
             </label>
             <Ul>
                 {tourns
-                    .filter(tourn => tourn.nameTime.toLowerCase().includes(query.toLowerCase()))
+                    .filter(tourn => tourn.nameTime.toLowerCase().includes(query.toLowerCase()) || tourn.editors.toLowerCase().includes(query.toLowerCase()))
                     .map(tourn => (
-                        <Li key={tourn.nameTime}>{tourn.nameTime}</Li>
+                        <Li key={tourn.nameTime}>
+                            <H>{tourn.nameTime}</H>
+                            <Info>
+                                {tourn.type} / {tourn.questions} / {tourn.price}
+                            </Info>
+                            <div>
+                                {tourn.editors
+                                    .split(')')
+                                    .slice(0, -1)
+                                    .map(edit => (
+                                        <Editor key={edit}>{edit ? edit + ')' : ''}</Editor>
+                                    ))}
+                            </div>
+                            <Links>
+                                <A href={tourn.link}>Link </A>
+                                {tourn.applicationAppealLink && <A href={tourn.applicationAppealLink}>Заявка/апель</A>}
+                            </Links>
+                        </Li>
                     ))}
             </Ul>
         </>
